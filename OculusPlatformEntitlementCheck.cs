3727e788f53ccb38a084527a86c193948c2bc215
Jorge <jores10@hotmail.com>
2018-08-30 16:44:05 -0600

    Add files via upload

---
+++ OculusPlatformEntitlementCheck.cs
@@ -0,0 +1,36 @@
+﻿//Code OculusPlatformEntitlementCheck 
+using UnityEngine;
+using Oculus.Platform;
+
+public class OculusPlatformEntitlementCheck : MonoBehaviour
+{
+
+    void Awake()
+    {
+        try
+        {
+            Core.AsyncInitialize();
+            Oculus.Platform.Entitlements.IsUserEntitledToApplication().OnComplete(GetEntitlementCallback);
+        }
+        catch (UnityException e)
+        {
+            Debug.LogError("Platform failed to initialize due to exception.");
+            Debug.LogException(e);
+            // Immediately quit the application.
+            UnityEngine.Application.Quit();
+        }
+    }
+
+    void GetEntitlementCallback(Message msg)
+    {
+        if (msg.IsError)
+        {
+            Debug.LogError("You are NOT entitled to use this app.");
+            UnityEngine.Application.Quit();
+        }
+        else
+        {
+            Debug.Log("You are entitled to use this app.");
+        }
+    }
+}
